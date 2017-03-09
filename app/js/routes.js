c508304fdf63ae7d59a836e6a93ada5fd03c3212
Sabrina <sabrina.mardjoeki@gmail.com>
2017-03-09 13:13:41 +0100

    Angularjs update

---
+++ app/js/routes.js
@@ -0,0 +1,15 @@
+angular.module('app')
+    .config(function($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('web', {
+                abstract: true,
+                template: '<ui-view/>'
+            })
+            .state('web.home', {
+                url: '/',
+                templateUrl: 'home.html',
+                controller:'MainController'
+            });
+
+        $urlRouterProvider.otherwise('/');
+    });
